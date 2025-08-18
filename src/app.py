80fb469907000eec40b1854158941b466c362507
Marcus Trommen <marcus.trommen@gmx.net>
2025-08-18 14:23:47 +0200

    optimized clickable area for link entry

--- src/app.py
+++ src/app.py
@@ -107,26 +107,6 @@ class Application:
 
 
     # -----------------------------------------------------------------------------
-    def _create_pdf_from_images(self) -> None:
-        """Create a PDF file from the images generated from the PDF files.
-        https://py-pdf.github.io/fpdf2"""
-        logger.debug("Creating PDF from images")
-
-
-        files = os.listdir(self.data["BUILD_DIR"])
-        files = sorted(files)
-        for file_name in files:
-            if file_name.endswith(".png"):
-                file_path = os.path.join(self.data["BUILD_DIR"], file_name)
-                if os.path.isfile(file_path):
-                    self.pdf_document.add_page()
-                    if file_name in self.data["PDF_DATA"]["toc"].keys():
-                        self.pdf_document.set_link(self.data["PDF_DATA"]["toc"][file_name], page=self.pdf_document.page_no())
-                    self.pdf_document.image(file_path, x=0, y=0, w=210, h=297)
-                    logger.debug(f"Image {file_name} added to PDF")
-
-
-    # -----------------------------------------------------------------------------
     def _generate_toc(self) -> None:
         """Generate a table of contents for the PDF document.
         This method creates a clickable TOC on the first page of the PDF."""
@@ -150,9 +130,15 @@ class Application:
         for item in self.data["PDF_DATA"]["file_list_to_merge"]:
             toc_entry = item["toc_entry"]
             target_page_num = item["target_page_num"]
+
+            # Create a link placeholder with a link_id for the TOC entry 
+            # and store it in the toc dictionary to link to the corresponding page later
+            # while adding the images to the PDF
             target_link = self.pdf_document.add_link()
             self.data["PDF_DATA"]["toc"][item["page_file_name"]] = target_link
 
+            # Calculate the width of the dots to fill the space between the TOC entry and the page number
+            # and create the TOC entry with clickable link
             toc_entry_width = self.pdf_document.get_string_width(toc_entry)
             page_number_width = self.pdf_document.get_string_width(str(target_page_num))
             double_space_width = self.pdf_document.get_string_width("  ")
@@ -162,8 +148,9 @@ class Application:
             dots = "." * dots_fill_count
 
             toc_text = f"{toc_entry} {dots}"
+            # make the TOC entry clickable including the dots but not the page number
             self.pdf_document.cell(w=0, h=10, txt=toc_text, ln=0, link=target_link, align="L")
-            self.pdf_document.cell(w=0, h=10, txt=str(target_page_num), ln=True, align="R")
+            self.pdf_document.cell(w=0, h=10, txt=str(target_page_num), link=target_link, ln=True, align="R")
             
         self.pdf_document.ln(10)
 
@@ -171,3 +158,25 @@ class Application:
         self.pdf_document.cell(w=0, h=10, txt=self.data["PDF_DATA"]["toc_hint"], ln=True, align="C")
 
         logger.debug("Table of contents generated successfully")
+
+
+    # -----------------------------------------------------------------------------
+    def _create_pdf_from_images(self) -> None:
+        """Create a PDF file from the images generated from the PDF files.
+        https://py-pdf.github.io/fpdf2"""
+        logger.debug("Creating PDF from images")
+
+
+        files = os.listdir(self.data["BUILD_DIR"])
+        files = sorted(files)
+        for file_name in files:
+            if file_name.endswith(".png"):
+                file_path = os.path.join(self.data["BUILD_DIR"], file_name)
+                if os.path.isfile(file_path):
+                    self.pdf_document.add_page()
+                    if file_name in self.data["PDF_DATA"]["toc"].keys():
+                        # set link target for the TOC entry placeholder
+                        # if the file is in the TOC and current page is the first page of the pdf document and the target page
+                        self.pdf_document.set_link(self.data["PDF_DATA"]["toc"][file_name], page=self.pdf_document.page_no())
+                    self.pdf_document.image(file_path, x=0, y=0, w=210, h=297)
+                    logger.debug(f"Image {file_name} added to PDF")
